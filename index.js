dee5e5ba416e1a12e39c7e96839a21b045e4373d
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-07 23:21:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+  var customerName= 'bob';
+function upperCaseCustomer(){
+  customerName='BOB';
+} 
+function setBestCustomer(){
+ var bestCustomer='not bob';
+}
+function overwriteBestCustomer(){
+  bestCustomer='maybe bob';
+}
+const leastFavouriteCustomer="jimmy"
+  function changeLeastFavouriteCustomer(leastfavourite){
+   leastFavouriteCustomer='bob'
+  }
+console.log (changeLeastFavouriteCustomer(leastFavouriteCustomer))
